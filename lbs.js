44b180eb65b238d9ce588051421c4e696bfafacb
Lily Misra <bigpumpkin236@outlook.com>
2020-12-29 11:37:24 -0600

    made changes

--- lbs.js
+++
@@ -1,8 +0,0 @@
-const form = document.querySelector("form");
-const span = form.querySelector("span");
-
-export default {
-  button: form.querySelector("button"),
-  charCount: span.querySelector("span"),
-  textarea: form.querySelector("textarea"),
-};
