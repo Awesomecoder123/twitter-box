b74b4ba3a19dbb0b842809fd3887a10f219dae43
Lily Misra <bigpumpkin236@outlook.com>
2020-12-31 09:49:21 -0600

    made changes to botton

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-import hey from "./lib.js";
+import dom from "./lib.js";
+
+dom.textarea.addEventListener("keyup", function handleTextarea() {
+  if (this.value.length) {
+    dom.button.disabled = false;
+  } else {
+    dom.button.disabled = true;
+  }
+});
